9e53f3cd3c5f6a32c883112e9069e6e2aaa20b75
Yusuf Bin Musa <yusuf.musa3@gmail.com>
2022-03-03 19:19:50 +0800

    fix: update prefix

--- src/main/java/seedu/address/logic/parser/RecipeBookSyntax.java
+++ src/main/java/seedu/address/logic/parser/RecipeBookSyntax.java
@@ -5,10 +5,11 @@ package seedu.address.logic.parser;
  */
 public class RecipeBookSyntax {
 
-    /* Prefix definitions */
+    /* Shorthand prefix */
     public static final Prefix PREFIX_NAME = new Prefix("-n");
     public static final Prefix PREFIX_INGREDIENT = new Prefix("-i");
+    public static final Prefix PREFIX_COMPLETION_TIME = new Prefix("-d");
+    public static final Prefix PREFIX_SERVING_SIZE = new Prefix("-ss:");
     public static final Prefix PREFIX_STEPS = new Prefix("-s");
-    public static final Prefix PREFIX_TAG = new Prefix("--tag");
-    public static final Prefix PREFIX_DURATION = new Prefix("--time");
+    public static final Prefix PREFIX_TAG = new Prefix("-t");
 }
