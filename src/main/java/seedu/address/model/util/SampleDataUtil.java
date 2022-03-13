60012757aa9b004668fe706b60e33426ff9a5978
blaketingyu <blaketingyu@gmail.com>
2022-03-14 02:56:39 +0800

    perf: reuse existing code from ParseIngredients to adhere to the DRY Principle

--- src/main/java/seedu/address/model/util/SampleDataUtil.java
+++ src/main/java/seedu/address/model/util/SampleDataUtil.java
@@ -1,6 +1,10 @@
 package seedu.address.model.util;
 
-import java.util.*;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.List;
+import java.util.Set;
 import java.util.stream.Collectors;
 
 import seedu.address.logic.parser.RecipeBookParserUtil;
