30af3d8ebbe0773d71e48ca1ca092c3b82c77905
Yusuf Bin Musa <yusuf.musa3@gmail.com>
2022-03-15 14:11:17 +0800

    feat!: implement ListCommand
    Closes #78

--- src/main/java/seedu/address/logic/parser/RecipeBookParser.java
+++ src/main/java/seedu/address/logic/parser/RecipeBookParser.java
@@ -11,6 +11,7 @@ import seedu.address.logic.commands.Command;
 import seedu.address.logic.commands.DeleteCommand;
 import seedu.address.logic.commands.FindCommand;
 import seedu.address.logic.commands.HelpCommand;
+import seedu.address.logic.commands.ListCommand;
 import seedu.address.logic.commands.ViewCommand;
 import seedu.address.logic.parser.exceptions.ParseException;
 
@@ -47,6 +48,8 @@ public class RecipeBookParser {
             return new DeleteCommandParser().parse(arguments);
         case FindCommand.COMMAND_WORD:
             return new FindCommandParser().parse(arguments);
+        case ListCommand.COMMAND_WORD:
+            return new ListCommand();
         case ViewCommand.COMMAND_WORD:
             return new ViewCommandParser().parse(arguments);
         default:
