211f31d45b47e2a0f9185842e029c9bbb255cc18
Yusuf Bin Musa <yusuf.musa3@gmail.com>
2022-03-17 13:59:52 +0800

    test: improve code coverage for SearchSet
    Closes #98

---
+++ src/test/java/seedu/address/model/recipe/SearchSetTest.java
@@ -0,0 +1,24 @@
+package seedu.address.model.recipe;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import java.util.List;
+
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+
+class SearchSetTest {
+
+    private static final SearchSet ss = new SearchSet();
+
+    @Test
+    void add_test() {
+        List<String> sampleInput = List.of("Western", "Korean", "Aglio Olio", "Bibimbap");
+        StringBuilder sb = new StringBuilder();
+        for (String s : sampleInput) {
+            ss.add(s);
+            sb.append(s).append(" ");
+        }
+        assertEquals(ss.toString(), sb.toString());
+    }
+}
