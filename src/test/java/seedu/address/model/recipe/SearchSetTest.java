e44dac9c1ba22d7d292447015c809aad4ad504c1
Yusuf Bin Musa <yusuf.musa3@gmail.com>
2022-03-17 15:08:35 +0800

    test: include equality check for consistency

--- src/test/java/seedu/address/model/recipe/SearchSetTest.java
+++ src/test/java/seedu/address/model/recipe/SearchSetTest.java
@@ -10,14 +10,15 @@ import org.junit.jupiter.api.Test;
 
 class SearchSetTest {
 
-    private final SearchSet ss = new SearchSet();
+    private SearchSet ss1 = new SearchSet();
     private final List<String> sampleInput = List.of("Western", "Korean", "Aglio Olio", "Bibimbap");
     private final StringBuilder sb = new StringBuilder();
 
     @BeforeEach
     void initialise() {
+        ss1 = new SearchSet();
         for (String s : sampleInput) {
-            ss.add(s);
+            ss1.add(s);
             appendString(sb, s);
         }
     }
@@ -25,20 +26,42 @@ class SearchSetTest {
     @Test
     void add_test_success() {
         String newValue = "Halal";
-        ss.add(newValue);
+        ss1.add(newValue);
         appendString(sb, newValue);
-        assertEquals(ss.toString(), sb.toString());
+        assertEquals(ss1.toString(), sb.toString());
     }
 
     @Test
     void add_value_exists() {
         String existingValue = "Western";
-        ss.add(existingValue);
+        ss1.add(existingValue);
         appendString(sb, existingValue);
-        assertNotEquals(ss.toString(), sb.toString());
+        assertNotEquals(ss1.toString(), sb.toString());
+    }
+
+    @Test
+    void equalityCheck() {
+        SearchSet ss1Copy = new SearchSet();
+        SearchSet ss2 = new SearchSet();
+        sampleInput.forEach(s -> {
+            ss1Copy.add(s);
+            ss2.add(s);
+        });
+        ss2.add("NEW ENTRY");
+
+        // SearchSet::equals
+        assertNotEquals(ss1, ss1.getSearchValues());
+        assertEquals(ss1, ss1);
+        assertEquals(ss1, ss1Copy);
+        assertNotEquals(ss1, ss2);
+
+        // SearchSet::hashCode
+        assertEquals(ss1.hashCode(), ss1Copy.hashCode());
+        assertNotEquals(ss1.hashCode(), ss2.hashCode());
     }
 
     private void appendString(StringBuilder sb, String s) {
         sb.append(s).append(" ");
     }
+
 }
