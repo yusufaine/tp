75e774c4e585651a4863121a874b9a8cf8a6c8ec
Yusuf Bin Musa <yusuf.musa3@gmail.com>
2022-03-17 14:51:32 +0800

    test: improve test by including invalid case
    Implement suggestion in PR from Blake. :+1:

--- src/test/java/seedu/address/model/recipe/SearchSetTest.java
+++ src/test/java/seedu/address/model/recipe/SearchSetTest.java
@@ -1,23 +1,44 @@
 package seedu.address.model.recipe;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotEquals;
 
 import java.util.List;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 class SearchSetTest {
 
-    private static final SearchSet ss = new SearchSet();
+    private final SearchSet ss = new SearchSet();
+    private final List<String> sampleInput = List.of("Western", "Korean", "Aglio Olio", "Bibimbap");
+    private final StringBuilder sb = new StringBuilder();
 
-    @Test
-    void add_test() {
-        List<String> sampleInput = List.of("Western", "Korean", "Aglio Olio", "Bibimbap");
-        StringBuilder sb = new StringBuilder();
+    @BeforeEach
+    void initialise() {
         for (String s : sampleInput) {
             ss.add(s);
-            sb.append(s).append(" ");
+            appendString(sb, s);
         }
+    }
+
+    @Test
+    void add_test_success() {
+        String newValue = "Halal";
+        ss.add(newValue);
+        appendString(sb, newValue);
         assertEquals(ss.toString(), sb.toString());
+    }
+
+    @Test
+    void add_value_exists() {
+        String existingValue = "Western";
+        ss.add(existingValue);
+        appendString(sb, existingValue);
+        assertNotEquals(ss.toString(), sb.toString());
+    }
+
+    private void appendString(StringBuilder sb, String s) {
+        sb.append(s).append(" ");
     }
 }
