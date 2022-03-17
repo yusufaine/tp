56af93a5f13cf180f6c8af3a75579a9a98486f87
gigituang <72374000+gigituang@users.noreply.github.com>
2022-03-17 20:13:42 +0800

    test: update CommandTestUtil
    update ExitCommandTest and ListCommandTest

--- src/test/java/seedu/address/logic/commands/CommandTestUtil.java
+++ src/test/java/seedu/address/logic/commands/CommandTestUtil.java
@@ -1,31 +1,32 @@
-//package seedu.address.logic.commands;
-//
-//import static org.junit.jupiter.api.Assertions.assertEquals;
-//import static org.junit.jupiter.api.Assertions.assertTrue;
-//import static seedu.address.logic.parser.RecipeBookSyntax.PREFIX_NAME;
-//import static seedu.address.logic.parser.RecipeBookSyntax.PREFIX_INGREDIENT;
-//import static seedu.address.logic.parser.RecipeBookSyntax.PREFIX_COMPLETION_TIME;
-//import static seedu.address.logic.parser.RecipeBookSyntax.PREFIX_SERVING_SIZE;
-//import static seedu.address.logic.parser.RecipeBookSyntax.PREFIX_STEP;
-//import static seedu.address.logic.parser.RecipeBookSyntax.PREFIX_TAG;
-//
-//import static seedu.address.testutil.Assert.assertThrows;
-//
-//import java.util.ArrayList;
-//import java.util.Arrays;
-//import java.util.List;
-//
-//import seedu.address.commons.core.index.Index;
-//import seedu.address.logic.commands.exceptions.CommandException;
-//import seedu.address.model.RecipeBook;
-//import seedu.address.model.Model;
-//import seedu.address.model.recipe.Recipe;
-//
-///**
-// * Contains helper methods for testing commands.
-// */
-//public class CommandTestUtil {
-//
+package seedu.address.logic.commands;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import static seedu.address.logic.parser.RecipeBookSyntax.PREFIX_NAME;
+import static seedu.address.logic.parser.RecipeBookSyntax.PREFIX_INGREDIENT;
+import static seedu.address.logic.parser.RecipeBookSyntax.PREFIX_COMPLETION_TIME;
+import static seedu.address.logic.parser.RecipeBookSyntax.PREFIX_SERVING_SIZE;
+import static seedu.address.logic.parser.RecipeBookSyntax.PREFIX_STEP;
+import static seedu.address.logic.parser.RecipeBookSyntax.PREFIX_TAG;
+
+import static seedu.address.testutil.Assert.assertThrows;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+import seedu.address.commons.core.index.Index;
+import seedu.address.logic.commands.exceptions.CommandException;
+import seedu.address.model.RecipeBook;
+import seedu.address.model.Model;
+import seedu.address.model.recipe.Recipe;
+import seedu.address.model.recipe.RecipeContainsKeywordPredicate;
+
+/**
+ * Contains helper methods for testing commands.
+ */
+public class CommandTestUtil {
+
 //    public static final String VALID_NAME_AMY = "Amy Bee";
 //    public static final String VALID_NAME_BOB = "Bob Choo";
 //    public static final String VALID_PHONE_AMY = "11111111";
@@ -53,10 +54,12 @@
 //    public static final String INVALID_EMAIL_DESC = " " + PREFIX_EMAIL + "bob!yahoo"; // missing '@' symbol
 //    public static final String INVALID_ADDRESS_DESC = " " + PREFIX_ADDRESS; // empty string not allowed for addresses
 //    public static final String INVALID_TAG_DESC = " " + PREFIX_TAG + "hubby*"; // '*' not allowed in tags
-//
+
 //    public static final String PREAMBLE_WHITESPACE = "\t  \r  \n";
 //    public static final String PREAMBLE_NON_EMPTY = "NonEmptyPreamble";
-//
+
+
+
 //    public static final EditCommand.EditPersonDescriptor DESC_AMY;
 //    public static final EditCommand.EditPersonDescriptor DESC_BOB;
 //
@@ -68,61 +71,61 @@
 //                .withPhone(VALID_PHONE_BOB).withEmail(VALID_EMAIL_BOB).withAddress(VALID_ADDRESS_BOB)
 //                .withTags(VALID_TAG_HUSBAND, VALID_TAG_FRIEND).build();
 //    }
-//
-//    /**
-//     * Executes the given {@code command}, confirms that <br>
-//     * - the returned {@link CommandResult} matches {@code expectedCommandResult} <br>
-//     * - the {@code actualModel} matches {@code expectedModel}
-//     */
-//    public static void assertCommandSuccess(Command command, Model actualModel, CommandResult expectedCommandResult,
-//                                            Model expectedModel) {
-//        try {
-//            CommandResult result = command.execute(actualModel);
-//            assertEquals(expectedCommandResult, result);
-//            assertEquals(expectedModel, actualModel);
-//        } catch (CommandException ce) {
-//            throw new AssertionError("Execution of command should not fail.", ce);
-//        }
-//    }
-//
-//    /**
-//     * Convenience wrapper to {@link #assertCommandSuccess(Command, Model, CommandResult, Model)}
-//     * that takes a string {@code expectedMessage}.
-//     */
-//    public static void assertCommandSuccess(Command command, Model actualModel, String expectedMessage,
-//                                            Model expectedModel) {
-//        CommandResult expectedCommandResult = new CommandResult(expectedMessage);
-//        assertCommandSuccess(command, actualModel, expectedCommandResult, expectedModel);
-//    }
-//
-//    /**
-//     * Executes the given {@code command}, confirms that <br>
-//     * - a {@code CommandException} is thrown <br>
-//     * - the CommandException message matches {@code expectedMessage} <br>
-//     * - the address book, filtered person list and selected person in {@code actualModel} remain unchanged
-//     */
-//    public static void assertCommandFailure(Command command, Model actualModel, String expectedMessage) {
-//        // we are unable to defensively copy the model for comparison later, so we can
-//        // only do so by copying its components.
-//        AddressBook expectedAddressBook = new AddressBook(actualModel.getAddressBook());
-//        List<Person> expectedFilteredList = new ArrayList<>(actualModel.getFilteredPersonList());
-//
-//        assertThrows(CommandException.class, expectedMessage, () -> command.execute(actualModel));
-//        assertEquals(expectedAddressBook, actualModel.getAddressBook());
-//        assertEquals(expectedFilteredList, actualModel.getFilteredPersonList());
-//    }
-//    /**
-//     * Updates {@code model}'s filtered list to show only the person at the given {@code targetIndex} in the
-//     * {@code model}'s address book.
-//     */
-//    public static void showPersonAtIndex(Model model, Index targetIndex) {
-//        assertTrue(targetIndex.getZeroBased() < model.getFilteredPersonList().size());
-//
-//        Person person = model.getFilteredPersonList().get(targetIndex.getZeroBased());
-//        final String[] splitName = person.getName().fullName.split("\\s+");
-//        model.updateFilteredPersonList(new NameContainsKeywordsPredicate(Arrays.asList(splitName[0])));
-//
-//        assertEquals(1, model.getFilteredPersonList().size());
-//    }
-//
-//}
+
+    /**
+     * Executes the given {@code command}, confirms that <br>
+     * - the returned {@link CommandResult} matches {@code expectedCommandResult} <br>
+     * - the {@code actualModel} matches {@code expectedModel}
+     */
+    public static void assertCommandSuccess(Command command, Model actualModel, CommandResult expectedCommandResult,
+                                            Model expectedModel) {
+        try {
+            CommandResult result = command.execute(actualModel);
+            assertEquals(expectedCommandResult, result);
+            assertEquals(expectedModel, actualModel);
+        } catch (CommandException ce) {
+            throw new AssertionError("Execution of command should not fail.", ce);
+        }
+    }
+
+    /**
+     * Convenience wrapper to {@link #assertCommandSuccess(Command, Model, CommandResult, Model)}
+     * that takes a string {@code expectedMessage}.
+     */
+    public static void assertCommandSuccess(Command command, Model actualModel, String expectedMessage,
+                                            Model expectedModel) {
+        CommandResult expectedCommandResult = new CommandResult(expectedMessage);
+        assertCommandSuccess(command, actualModel, expectedCommandResult, expectedModel);
+    }
+
+    /**
+     * Executes the given {@code command}, confirms that <br>
+     * - a {@code CommandException} is thrown <br>
+     * - the CommandException message matches {@code expectedMessage} <br>
+     * - the address book, filtered person list and selected person in {@code actualModel} remain unchanged
+     */
+    public static void assertCommandFailure(Command command, Model actualModel, String expectedMessage) {
+        // we are unable to defensively copy the model for comparison later, so we can
+        // only do so by copying its components.
+        RecipeBook expectedAddressBook = new RecipeBook(actualModel.getRecipeBook());
+        List<Recipe> expectedFilteredList = new ArrayList<>(actualModel.getFilteredRecipeList());
+
+        assertThrows(CommandException.class, expectedMessage, () -> command.execute(actualModel));
+        assertEquals(expectedAddressBook, actualModel.getRecipeBook());
+        assertEquals(expectedFilteredList, actualModel.getFilteredRecipeList());
+    }
+    /**
+     * Updates {@code model}'s filtered list to show only the person at the given {@code targetIndex} in the
+     * {@code model}'s address book.
+     */
+    public static void showRecipeAtIndex(Model model, Index targetIndex) {
+        assertTrue(targetIndex.getZeroBased() < model.getFilteredRecipeList().size());
+
+        Recipe recipe = model.getFilteredRecipeList().get(targetIndex.getZeroBased());
+        final String[] splitName = recipe.getName().fullName.split("\\s+");
+        model.updateFilteredRecipeList(new RecipeContainsKeywordPredicate(Arrays.asList(splitName[0])));
+
+        assertEquals(1, model.getFilteredRecipeList().size());
+    }
+
+}
