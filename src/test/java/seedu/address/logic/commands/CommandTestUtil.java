3046f838f984b87881e81a76e9f6bca445d72751
Ryan Aidan <aidanaden@hotmail.com>
2022-03-21 23:46:54 +0800

    Remove unused commented recipe examples from AB3
    Co-authored-by: Yusuf Bin Musa <29030941+yusufaine@users.noreply.github.com>

--- src/test/java/seedu/address/logic/commands/CommandTestUtil.java
+++ src/test/java/seedu/address/logic/commands/CommandTestUtil.java
@@ -73,13 +73,6 @@ public class CommandTestUtil {
     public static final String STEP_DESC_CHICKEN_CHOP = " " + PREFIX_STEP + VALID_STEP_1_CHICKEN_CHOP;
     public static final String TAG_DESC_AGLIO_OLIO = " " + PREFIX_TAG + VALID_TAG_AGLIO_OLIO;
     public static final String TAG_DESC_CHICKEN_CHOP = " " + PREFIX_TAG + VALID_TAG_CHICKEN_CHOP;
-    //
-    // public static final String INVALID_NAME_DESC = " " + PREFIX_NAME + "James&"; // '&' not allowed in names
-    // public static final String INVALID_PHONE_DESC = " " + PREFIX_PHONE + "911a"; // 'a' not allowed in phones
-    // public static final String INVALID_EMAIL_DESC = " " + PREFIX_EMAIL + "bob!yahoo"; // missing '@' symbol
-    // public static final String INVALID_ADDRESS_DESC = " " + PREFIX_ADDRESS; // empty string not allowed for addresses
-    // public static final String INVALID_TAG_DESC = " " + PREFIX_TAG + "hubby*"; // '*' not allowed in tags
-    //
     public static final String PREAMBLE_WHITESPACE = "\t  \r  \n";
     public static final String PREAMBLE_NON_EMPTY = "NonEmptyPreamble";
 
