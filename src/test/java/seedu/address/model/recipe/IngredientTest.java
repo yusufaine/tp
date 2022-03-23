c715428b8abb96394ffa512ea78c30cbb91d9950
gigituang <72374000+gigituang@users.noreply.github.com>
2022-03-23 16:11:29 +0800

    fixed: method names

--- src/test/java/seedu/address/model/recipe/IngredientTest.java
+++ src/test/java/seedu/address/model/recipe/IngredientTest.java
@@ -28,15 +28,6 @@ class IngredientTest {
                 new Ingredient(invalidIngredientName, validIngredientQty, validIngredientQuantifier));
     }
 
-    // @Test
-    // public void constructor_nullIngredientQty_throwsIllegalArgumentException() {
-    //     String invalidIngredientName = "Spaghetti";
-    //     double nullIngredientQty = null;
-    //     String validIngredientQuantifier = "g";
-    //     assertThrows(IllegalArgumentException.class,
-    //             () -> new Ingredient(invalidIngredientName, nullIngredientQty, validIngredientQuantifier));
-    // }
-
     @Test
     public void constructor_invalidIngredientQty_throwsIllegalArgumentException() {
         String validIngredientName = "Spaghetti";
@@ -47,7 +38,7 @@ class IngredientTest {
     }
 
     @Test
-    public void constructor_nullIngredientQuantifier_throwsIllegalArgumentException() {
+    public void constructor_nullIngredientQuantifier_isValid() {
         String invalidIngredientName = "Spaghetti";
         double validIngredientQty = 5.5;
         String nullIngredientQuantifier = null;
@@ -64,23 +55,15 @@ class IngredientTest {
                 new Ingredient(nullIngredientName, validIngredientQty, validIngredientQuantifier));
     }
 
-    // @Test
-    // public void constructor_invalidNullIngredientQuantifier_throwsIllegalArgumentException() {
-    //     String invalidIngredientName = "Spaghetti";
-    //     double validIngredientQty = 5.5;
-    //     String invalidIngredientQuantifier = null;
-    //     assertThrows(NullPointerException.class,
-    //             () -> new Ingredient(invalidIngredientName, validIngredientQty, invalidIngredientQuantifier));
-    // }
 
-    // @Test
-    // public void constructor_invalidNegativeIngredientQty_throwsIllegalArgumentException() {
-    //     String invalidIngredientName = "Spaghetti";
-    //     String validIngredientQty = "-12.4";
-    //     String validIngredientQuantifier = "g";
-    //     assertThrows(IllegalArgumentException.class,
-    //             () -> new Ingredient(invalidIngredientName, validIngredientQty, validIngredientQuantifier));
-    // }
+    @Test
+    public void constructor_invalidNegativeIngredientQty_throwsIllegalArgumentException() {
+        String invalidIngredientName = "Spaghetti";
+        double invalidIngredientQty = -12.4;
+        String validIngredientQuantifier = "g";
+        assertThrows(IllegalArgumentException.class, () ->
+                new Ingredient(invalidIngredientName, invalidIngredientQty, validIngredientQuantifier));
+    }
 
     @Test
     public void isValidIngredient() {
@@ -100,10 +83,6 @@ class IngredientTest {
         assertFalse(Ingredient.isValidIngredientName("-1")); // negative value only
         assertFalse(Ingredient.isValidIngredientName("-24.23")); // negative double only
 
-        // invalid ingredient quantifier
-        // assertFalse(Ingredient.isValidQuantifier("")); // empty string
-        // assertFalse(Ingredient.isValidQuantifier(" ")); // spaces only
-        // assertFalse(Ingredient.isValidQuantifier("       ")); // lots of spaces only
 
         // valid ingredient name
         assertTrue(Ingredient.isValidIngredientName("Spaghetti")); // 1 word ingredient
@@ -116,10 +95,6 @@ class IngredientTest {
         assertTrue(Ingredient.isValidQuantity(2.5)); // 1 decimal place double
         assertTrue(Ingredient.isValidQuantity(2.75)); // 2 decimal place double
 
-        // valid ingredient quantifier
-        // assertTrue(Ingredient.isValidQuantifier("kg")); // 2 letter quantifier
-        // assertTrue(Ingredient.isValidQuantifier("clovers")); // 1 word quantifier
-        // assertTrue(Ingredient.isValidQuantifier("metric tons")); // 2 word quantifier
     }
 
     @Test
