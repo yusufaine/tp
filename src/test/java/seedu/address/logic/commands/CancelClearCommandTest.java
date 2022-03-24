80fb73e752050e2bec9a7906ae85f4e1422d231d
Blake Tan <77196632+blaketingyu@users.noreply.github.com>
2022-03-24 08:37:20 +0800

    Update src/test/java/seedu/address/logic/commands/CancelClearCommandTest.java
    Co-authored-by: Ryan Aidan  <aidanaden@hotmail.com>

--- src/test/java/seedu/address/logic/commands/CancelClearCommandTest.java
+++ src/test/java/seedu/address/logic/commands/CancelClearCommandTest.java
@@ -7,7 +7,7 @@ import static seedu.address.testutil.TypicalRecipes.getTypicalRecipeBook;
 
 import org.junit.jupiter.api.Test;
 import seedu.address.model.Model;
-import seedu.address.model.ModelManager;
+import seedu.address.model.RecipeBookModelManager;
 import seedu.address.model.UserPrefs;
 
 /**
